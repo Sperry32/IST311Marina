a29b348009957857668dd0810925d745d18fc541
Sperry32 <stupac31@gmail.com>
2017-11-29 19:57:15 -0500

    Add files via upload

--- MarinaDatabase.java
+++ MarinaDatabase.java
@@ -31,12 +31,12 @@ public class MarinaDatabase {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Database Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     public ResultSet searchCustByName(String fName, String lName) {
         try
         {
             searchCustByName = connection.prepareStatement(
-                    "SELECT firstName, lastName, boatingLicense FROM Customer WHERE firstName = ? AND lastName = ?");
+                    "SELECT * FROM Customer WHERE firstName = ? AND lastName = ?");
 
             searchCustByName.setString(1, fName);
             searchCustByName.setString(2, lName);
@@ -52,15 +52,15 @@ public class MarinaDatabase {
             return rSet;
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public ResultSet searchPowerBoatByRegNum(int registrationNumber) {
         try
         {
             searchPowerBoatByRegNum = connection.prepareStatement(
-                    "SELECT registrationNumber, size, fuelType, numEngines, engineType, FROM Powerboat WHERE registrationNumber = ?");
+                    "SELECT * FROM Powerboat WHERE registrationNumber = ?");
 
             searchPowerBoatByRegNum.setInt(1, registrationNumber);
-            
+
 
             rSet = searchPowerBoatByRegNum.executeQuery();
 
@@ -73,15 +73,15 @@ public class MarinaDatabase {
             return rSet;
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
-     public ResultSet searchSailBoatByRegNum(int registrationNumber) {
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    public ResultSet searchSailBoatByRegNum(int registrationNumber) {
         try
         {
             searchSailBoatByRegNum = connection.prepareStatement(
-                    "SELECT registrationNumber, size, fuelType, keelHeight, hasEngine, sailNum FROM Sailboat WHERE registrationNumber = ?");
+                    "SELECT * FROM Sailboat WHERE registrationNumber = ?");
 
             searchSailBoatByRegNum.setInt(1, registrationNumber);
-            
+
 
             rSet = searchSailBoatByRegNum.executeQuery();
 
@@ -94,15 +94,15 @@ public class MarinaDatabase {
             return rSet;
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public ResultSet searchLeaseBySlipNum(int slipNum) {
         try
         {
-            searchCustByName = connection.prepareStatement(
-                    "SELECT slipNumber, slotNumber, duration, rate, expirationDate FROM Lease WHERE slipNumber = ?");
+            searchLeaseBySlipNum = connection.prepareStatement(
+                    "SELECT * FROM Lease WHERE slipNumber = ?");
 
-            searchLeaseBySlipNum.setInt(1, slipNumber);
-           
+            searchLeaseBySlipNum.setInt(1, slipNum);
+
 
             rSet = searchLeaseBySlipNum.executeQuery();
 
@@ -115,14 +115,14 @@ public class MarinaDatabase {
             return rSet;
         }
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public ResultSet searchMaintenanceByInvoiceNumber(int invoiceNumber) {
         try
         {
             searchMaintenanceByInvoiceNumber = connection.prepareStatement(
-                    "SELECT invoiceNum, dateOfMaintenance, workDone FROM Maintenance WHERE invoiceNum = ?");
+                    "SELECT * FROM Maintenance WHERE invoiceNumber = ?");
 
-          searchMaintenanceByInvoiceNumber.setInt(1, invoiceNumber);
+            searchMaintenanceByInvoiceNumber.setInt(1, invoiceNumber);
 
             rSet = searchMaintenanceByInvoiceNumber.executeQuery();
 
@@ -135,7 +135,7 @@ public class MarinaDatabase {
             return rSet;
         }
     }
-///////////////////////////////////////// ADDS////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////// ADDS////////////////////////////////////////////////////////////////////////
     public void addNewCust(int license, String fName, String lName){
         try{
             addNewCust = connection.prepareStatement("INSERT INTO CUSTOMER(boatingLicense, firstName, lastName) VALUES(?, ?, ?)");
@@ -151,17 +151,17 @@ public class MarinaDatabase {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public void addNewPowerboat(int registrationNumber, double size, String fuelType, int numEngines, String engineType){
         try{
             addNewPowerboat = connection.prepareStatement("INSERT INTO POWERBOAT(registrationNum, size, fuelType, numEngines, engineType) VALUES(?, ?, ?, ?, ?)");
 
-            addNewPowerboat.setInt(1, registrationNum);
+            addNewPowerboat.setInt(1, registrationNumber);
             addNewPowerboat.setDouble(2, size);
             addNewPowerboat.setString(3, fuelType);
             addNewPowerboat.setInt(4, numEngines);
             addNewPowerboat.setString(5, engineType);
-           
+
 
             System.out.println("Power Boat added!");
             int ans = addNewPowerboat.executeUpdate();
@@ -171,18 +171,18 @@ public class MarinaDatabase {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public void addNewSailboat(int registrationNumber, double size, String fuelType, double keelHeight, boolean hasEngine, int sailNum){
         try{
             addNewSailboat = connection.prepareStatement("INSERT INTO SAILBOAT(registrationNum, size, fuelType, keelHeight, hasEngine, sailNum) VALUES(?, ?, ?, ?, ?, ?)");
 
-            addNewSailboat.setInt(1, registrationNum);
+            addNewSailboat.setInt(1, registrationNumber);
             addNewSailboat.setDouble(2, size);
             addNewSailboat.setString(3, fuelType);
             addNewSailboat.setDouble(4, keelHeight);
             addNewSailboat.setBoolean(5, hasEngine);
             addNewSailboat.setInt(6, sailNum);
-           
+
 
             System.out.println("Sail Boat added!");
             int ans = addNewSailboat.executeUpdate();
@@ -192,7 +192,7 @@ public class MarinaDatabase {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public void addNewLease(int slipNumber, int slotNumber, int duration, double rate, String expirationDate, int registrationNumber, int boatingLicense){
         try{
             addNewLease = connection.prepareStatement("INSERT INTO LEASE(slipNumber, slotNumber, duration, rate, expirationDate) VALUES(?, ?, ?, ?, ?)");
@@ -202,42 +202,40 @@ public class MarinaDatabase {
             addNewLease.setInt(3, duration);
             addNewLease.setDouble(4, rate);
             addNewLease.setString(5, expirationDate);
-            
+
             System.out.println("Lease Created!");
             int ans = addNewLease.executeUpdate();
-            
-            
+
+
             /*
             addNewboatLeases = connection.prepareStatement("INSERT INTO BOATLEASES(slipNumber, registrationNumber) VALUES(?, ?)");
-
             addNewboatLeases.setInt(1, slipNumber);
             addNewboatLeases.setInt(2, registrationNumber);
-           
-            
+
+
             //System.out.println("Lease Created!");
             int ans2 = addNewLease.executeUpdate();
             */
-           
+
              /*
             addNewcustomerLeases = connection.prepareStatement("INSERT INTO CUSTOMERLEASES(boatingLicense, slipNumber) VALUES(?, ?)");
-
             addNewboatLeases.setInt(1, boatingLicense);
             addNewboatLeases.setInt(2, slipNumber);
-           
-            
+
+
             //System.out.println("Lease Created!");
             int ans3 = addNewLease.executeUpdate();
             */
-                
+
             //possible location for boatLeases and customerLeases code
-            
+
         }
         catch(Exception e){
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
-///////////////////////////////////////////////////////////////////////////////////////////////////////    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////
     public void addNewMaintenance(int invoiceNum, String dateOfMaintenance, String workDone, int registrationNumber){
         try{
             addNewMaintenance = connection.prepareStatement("INSERT INTO MAINTENANCE(invoiceNum, dateOfMaintenance, workDone) VALUES(?, ?, ?)");
@@ -248,18 +246,16 @@ public class MarinaDatabase {
 
             System.out.println("Maintenance record created!");
             int ans = addNewMaintenance.executeUpdate();
-            
+
            /*
             addNewboatMaintenance = connection.prepareStatement("INSERT INTO BOATMAINTENANCE(invoiceNum, registrationNumber) VALUES(?, ?, ?)");
-
             addNewMaintenance.setInt(1, invoiceNum);
             addNewMaintenance.setInt(2, registrationNumber);
-           
 
             //System.out.println("Maintenance record created!");
             int ans2 = addNewMaintenance.executeUpdate();
            */
-            
+
             //possible location for boatMaintenance code
         }
         catch(Exception e){
